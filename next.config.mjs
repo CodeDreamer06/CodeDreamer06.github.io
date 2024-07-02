7b12e840eb4f42ee39423b67154c96e79865123e
Abhinav <codedreamer06@gmail.com>
2024-07-02 16:17:38 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+  
+    // /**
+    //  * Set base path. This is the slug of your GitHub repository.
+    //  *
+    //  * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+    //  */
+    // basePath: "/nextjs-github-pages",
+    images: {
+      loader: "akamai",
+      path: "",
+    },
+    assetPrefix: "./",
+  };
+  
+  export default nextConfig;
